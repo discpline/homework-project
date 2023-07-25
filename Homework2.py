df4ed035d339f96381c0307c7f5212bebad5427e
discpline <dimoxernavse@gmail.com>
2023-07-25 17:03:19 +0300

    Homework2

--- Homework2.py
+++ Homework2.py
@@ -2,9 +2,9 @@ import math
 side_a = 0.3
 side_b = 0.4
 side_c = 0.5
-c = (side_a+side_b+side_c)/2
-Plosh4ad = math.sqrt(c*(c-side_a)*(c-side_b)*(c-side_c))
-print(c)
+Poly_perimetr = (side_a+side_b+side_c)/2
+Plosh4ad = math.sqrt(Poly_perimetr*(Poly_perimetr-side_a)*(Poly_perimetr-side_b)*(Poly_perimetr-side_c))
+print(Poly_perimetr)
 print(Plosh4ad)
 
 
