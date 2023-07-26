832c90e4f3355886ee8bae2400ceaa5f76df89e2
discpline <dimoxernavse@gmail.com>
2023-07-26 16:08:46 +0300

    Homework2

--- Homework2.py
+++ Homework2.py
@@ -1,11 +1,12 @@
 import math
-side_a = 0.3 #сторона 1
-side_b = 0.4 #сторона 2 
-side_c = 0.5 #сторона 3
-Poly_perimetr = ("Полу-периметр = ", side_a+side_b+side_c)/2 #нахождение полу-периметра по формуле
-Plosh4ad = math.sqrt("Площадь = ",Poly_perimetr*(Poly_perimetr-side_a)*(Poly_perimetr-side_b)*(Poly_perimetr-side_c)) #нахождение площади по формуле
-print(Poly_perimetr)
-print(Plosh4ad)
+side_a = 0.3  # сторона 1
+side_b = 0.4  # сторона 2
+side_c = 0.5  # сторона 3
+poly_perimeter = (side_a + side_b + side_c) / 2  # нахождение полу-периметра по формуле
+ploshchad = math.sqrt(poly_perimeter * (poly_perimeter - side_a) * (poly_perimeter - side_b) * (poly_perimeter - side_c))  # нахождение площади по формуле
+print("Полу-периметр =", poly_perimeter)
+print("Площадь =", ploshchad)
+
 
 
 
